e2dc7ef41149f2eae77bafe468c6e89a74e86ec3
Jonathan Simpson <jonathansimpson101@gmail.com>
2021-06-03 09:43:51 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+const chalk = require('chalk');
+const prompt = require('prompt-sync')();
+require('dotenv').config();
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+// Privide phone numbers
+const sandboxNumber = '+14155238886';
+const myNumber = '+447814277434';
+
+// Prompt to get number input
+let number = prompt(chalk.bgYellow('What number would you like a fact about?'));
+
+// Hit numbers API
+const url = `http://numbersapi.com/${number}/math?json`;
+
+fetch(url).then(response => response.json())
+          .then(result => {
+
+            // Present the result to the screen
+            console.log(chalk.bgGreen(result.text));
+
+
+            // Send fact via Whatsapp
+            client.messages.create({
+              from: `whatsapp:${sandboxNumber}`,
+              body: result.text,
+              to: `whatsapp:${myNumber}`
+            })
+
+
+          })
